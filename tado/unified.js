592ba2a824839002850f7062886c44be673e4eac
Ben Robinson <4210111+bsrobinson@users.noreply.github.com>
2026-06-22 20:43:24 +0100

    Fix Fan/Dry mode, removing temp value

--- tado/unified.js
+++ tado/unified.js
@@ -196,11 +196,13 @@ module.exports = {
 		
 		overlay.setting.mode = state.mode
 
-		if (!state.targetTemperature)
-			state.targetTemperature = 25
-		overlay.setting.temperature = {
-			fahrenheit: toFahrenheit(state.targetTemperature),
-			celsius: state.targetTemperature
+		if ('temperatures' in device.capabilities[state.mode]) {
+			if (!state.targetTemperature)
+				state.targetTemperature = 25
+			overlay.setting.temperature = {
+				fahrenheit: toFahrenheit(state.targetTemperature),
+				celsius: state.targetTemperature
+			}
 		}
 		
 		if ('swing' in device.capabilities[state.mode])
